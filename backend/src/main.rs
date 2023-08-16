cdd99b50076b9dd1d6f8e18cfa7d9d8748c497ba
Logan Cammish <124637378+logancammish@users.noreply.github.com>
2023-08-16 13:23:47 +1200

    Add files via upload

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,43 +1,48 @@
-use std::{future::{Future, Ready}, pin::Pin};
-
-use firebase_rs::{*};
+use firebase_rs::{ * };
 use serde::{Serialize, Deserialize};
-
 
 #[derive(Serialize, Deserialize, Debug)]
 struct User {
-    name: [String; 3],
+    name: String,
     password: String, 
     email: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]  
-struct Response {
+struct StringResponse {
     name: String,
 }
 
-
-async fn set_user(firebase: &firebase_rs::Firebase, user: &User) -> Response {
-    let _user = firebase.set::<User>(&user).await;
-    return string_to_response(String::from(&_user.unwrap().data)).await;
+struct JSON;
+impl JSON {
+    fn to_string(string: &str) -> StringResponse {
+        return serde_json::from_str(string).expect("There was a problem 
+                                            converting string data to a JSON objecet");
+    }
 }
 
-async fn string_to_response(string: String) -> Response {
-   serde_json::from_str::<Response>(string.as_str()).unwrap()
+struct Users;
+impl Users {
+    async fn create_account(firebase_client: &Firebase, user: &User) -> StringResponse {
+        let _user = firebase_client.at("users").set::<User>(&user).await;
+        return JSON::to_string(&_user.unwrap().data);
+    }
+
+    async fn delete_accout(firebase_client: &Firebase, user: &String) -> &str {
+        let _user = firebase_client.at("users").delete(&user).await;
+        return JSON::to_string(&_user.unwrap().data);
+    }
 }
+
 
 #[tokio::main] 
 async fn main() {
     let user = User { 
-        name: [
-            String::from("One"),
-            String::from("Two"),
-            String::from("Three")
-            ],
-        password: String::from("Four"),
-        email: String::from("Five")
+        name: String::from("one"),
+        password: String::from("two"),
+        email: String::from("three")
      };
-    let firebase: firebase_rs::Firebase = Firebase::auth("https://music-site-alt.firebaseapp.com", "*").unwrap().at("two");
-    println!("{}",set_user(&firebase, &user).await.name);
+    let firebase: firebase_rs::Firebase = Firebase::new("").unwrap().at("two");
+    println!("{}", set_user(&firebase, &user).await.name);
 }
 

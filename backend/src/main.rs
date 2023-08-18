64066877d8020226400de4b3df5375c41914bac3
Logan Cammish <124637378+logancammish@users.noreply.github.com>
2023-08-18 14:00:28 +1200

    Add files via upload

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,48 +1,39 @@
+mod dependency;
+use dependency::{ * };
 use firebase_rs::{ * };
-use serde::{Serialize, Deserialize};
-
-#[derive(Serialize, Deserialize, Debug)]
-struct User {
-    name: String,
-    password: String, 
-    email: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]  
-struct StringResponse {
-    name: String,
-}
-
-struct JSON;
-impl JSON {
-    fn to_string(string: &str) -> StringResponse {
-        return serde_json::from_str(string).expect("There was a problem 
-                                            converting string data to a JSON objecet");
-    }
-}
-
-struct Users;
-impl Users {
-    async fn create_account(firebase_client: &Firebase, user: &User) -> StringResponse {
-        let _user = firebase_client.at("users").set::<User>(&user).await;
-        return JSON::to_string(&_user.unwrap().data);
-    }
-
-    async fn delete_accout(firebase_client: &Firebase, user: &String) -> &str {
-        let _user = firebase_client.at("users").delete(&user).await;
-        return JSON::to_string(&_user.unwrap().data);
-    }
-}
-
+use bcrypt::{hash, verify};
+use std::{time::Duration, collections::HashMap};
+use settimeout::set_timeout;
 
 #[tokio::main] 
 async fn main() {
     let user = User { 
-        name: String::from("one"),
-        password: String::from("two"),
-        email: String::from("three")
-     };
-    let firebase: firebase_rs::Firebase = Firebase::new("").unwrap().at("two");
-    println!("{}", set_user(&firebase, &user).await.name);
+        name: String::from("name"),
+        password: hash(String::from("password") ,10).unwrap(), // use bcrypt to hash the password
+        email: String::from("name"),
+        playlists: Vec::from([
+            Playlist {
+                name: "Copyright Free".to_string(),
+                author: "name".to_string(),
+                cover: "random".to_string(),
+            },
+        ])
+    }; // user account information
+    let firebase: firebase_rs::Firebase = Firebase::new(
+        "https://.firebaseio.com").unwrap().at("users"); // open users directory in
+                                                                                  // realtime database
+    let user_id = Users::create_account(&firebase, &user).await; // create user account and take
+                                                                                 // stored id 
+    
+    println!("{}", verify(String::from("password"), user.password.as_str()).unwrap()); // verify password versus
+                                                                                    // encrypted version
+                                                                                      
+
+    println!("User id: {}", user_id.name);
+    println!("{}",&user_id.name);
+    println!("Info: {:?}", Users::get_info(&firebase, &user_id.name).await.playlists);
+    set_timeout(Duration::from_secs(5)).await; // wait 5 seconds
+    Users::delete_account(&firebase, &user_id.name).await; // delete the account
+    println!("Deleted account: {}", user_id.name);
 }
 

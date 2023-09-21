a66608dbb50f3e56f10c549901a517e2657f7b09
Logan Cammish <124637378+logancammish@users.noreply.github.com>
2023-09-22 10:23:41 +1200

    Add files via upload

--- backend/src/dependency.rs
+++ backend/src/dependency.rs
@@ -1,7 +1,7 @@
-use firebase_rs::{ * };
-use std::collections::HashMap;
+use bcrypt::{hash, verify};
+use firebase_rs::Firebase;
 use serde::{Serialize, Deserialize};
-
+use settimeout::set_timeout;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct User {
@@ -23,14 +23,6 @@ pub struct Playlist {
     pub cover: String,
 }
 
-pub struct JSON;
-impl JSON {
-    pub fn to_string(string: &str) -> StringResponse {
-        return serde_json::from_str(string).expect("There was a problem 
-                                            converting string data to a JSON objecet");
-    }
-}
-
 pub struct Users;
 impl Users {
     pub async fn create_account(firebase_client: &Firebase, user: &User) -> StringResponse {
@@ -47,5 +39,78 @@ impl Users {
         let _user = firebase_client.at(&userid);
         let user = _user.get::<User>().await.unwrap();
         return user;
+    }
+}
+
+
+
+pub struct JSON;
+impl JSON {
+    pub fn to_string(string: &str) -> StringResponse {
+        return serde_json::from_str(string).expect("There was a problem 
+                                            converting string data to a JSON objecet");
+    }
+}
+
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream, Ipv4Addr, IpAddr}, fs, time::Duration, borrow::{BorrowMut, Borrow},
+};
+use chrono::{Utc}; 
+const PATH: &str = "C:/Users//Documents/music-website/frontend";
+
+pub struct WebServer;
+impl WebServer {
+    pub async fn establish() {
+        let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+        for stream in listener.incoming() {
+            let c_stream = stream.unwrap();
+            println!("{} : Connection established", Utc::now());
+
+            WebServer::handle_connection(c_stream).await;
+        }
+    }
+
+    async fn handle_connection(mut stream: TcpStream) {
+        let ip = stream.local_addr().unwrap().ip();
+        println!("{} | {:?} Connected", Utc::now(), ip);
+
+        let buf_reader = BufReader::new(&mut stream);
+        let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+        if request_line == "GET / HTTP/1.1" {            
+            let user = User { 
+                name: String::from("name"),
+                password: hash(String::from("password") ,10).unwrap(), 
+                email: String::from("email"),
+                playlists: Vec::from([
+                    Playlist {
+                        name: "Copyright Free".to_string(),
+                        author: "name".to_string(),
+                        cover: "random".to_string(),
+                    },
+                    Playlist {
+                        name: "fusdijkof".to_string(),
+                        author: "sjdfks".to_string(),
+                        cover: "random".to_string()
+                    }
+                ])
+            }; 
+            let firebase: firebase_rs::Firebase = Firebase::new(
+                "https://x.firebaseio.com").unwrap().at("users");
+            let user_id = Users::create_account(&firebase, &user).await;            
+            //println!("USER PASSWORD CORRECT: {}", verify(String::from("password"), user.password.as_str()).unwrap());                                                                                            
+            println!("{} | {} / USER ACCOUNT CREATED: {} / DATA: {:?}", Utc::now(), ip, user_id.name,
+                Users::get_info(&firebase, &user_id.name).await.playlists);
+            set_timeout(Duration::from_secs(1)).await; 
+
+            let contents = format!("[ID: \"{}\"]",user_id.name.as_str()); 
+            let length = contents.len();
+            let response = format!(
+                "HTTP/1.1 200 OK\r\nContent-Length: {length}\r\n\r\n{contents}"
+            );
+
+            stream.write_all(response.as_bytes()).unwrap();
+        } 
     }
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,11 +2,14 @@ mod dependency;
 use dependency::{ * };
 use firebase_rs::{ * };
 use bcrypt::{hash, verify};
-use std::{time::Duration, collections::HashMap};
+use std::time::Duration;
 use settimeout::set_timeout;
 
 #[tokio::main] 
 async fn main() {
+    WebServer::establish().await;
+    
+/*
     let user = User { 
         name: String::from("name"),
         password: hash(String::from("password") ,10).unwrap(), // use bcrypt to hash the password
@@ -17,10 +20,15 @@ async fn main() {
                 author: "name".to_string(),
                 cover: "random".to_string(),
             },
+            Playlist {
+                name: "fusdijkof".to_string(),
+                author: "sjdfks".to_string(),
+                cover: "random".to_string()
+            }
         ])
-    }; // user account information
+    }; // user accoun6t information
     let firebase: firebase_rs::Firebase = Firebase::new(
-        "https://.firebaseio.com").unwrap().at("users"); // open users directory in
+        "https://music-site-alt-default-rtdb.firebaseio.com").unwrap().at("users"); // open users directory in
                                                                                   // realtime database
     let user_id = Users::create_account(&firebase, &user).await; // create user account and take
                                                                                  // stored id 
@@ -34,6 +42,6 @@ async fn main() {
     println!("Info: {:?}", Users::get_info(&firebase, &user_id.name).await.playlists);
     set_timeout(Duration::from_secs(5)).await; // wait 5 seconds
     Users::delete_account(&firebase, &user_id.name).await; // delete the account
-    println!("Deleted account: {}", user_id.name);
+    println!("Deleted account: {}", user_id.name);*/
 }
 
